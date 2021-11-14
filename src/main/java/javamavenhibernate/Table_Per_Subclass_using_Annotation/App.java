30155423b16b3c1a4047ece90bd4bd0e51b33f1a
SreelakshmiSajeevan <54982096+SreelakshmiSajeevan@users.noreply.github.com>
2021-11-14 16:23:44 +0530

    Add files via upload

---
+++ src/main/java/javamavenhibernate/Table_Per_Subclass_using_Annotation/App.java
@@ -0,0 +1,13 @@
+package javamavenhibernate.Table_Per_Subclass_using_Annotation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
